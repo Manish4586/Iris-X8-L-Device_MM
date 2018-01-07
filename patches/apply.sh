ec1c0e787978a2d7be446b25c745f61274458985
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-07 11:30:35 +0530

    Add files via upload

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,7 +1,38 @@
-#!/bin/bash
-
-cd ../..
-cd apps/Settings
-patch -p1 -b < ../../../device/Lava/IrisX8/patches/dev_info.patch
-
-echo You Did It! Manish_4586!
+ #!/bin/bash
+ cd ../../../..
+ cd system/core
+ patch -p1 < ../../device/Lava/IrisX8/patches/core.patch
+ cd ../..
+ cd system*/bt
+ patch -p1 < ../../device/Lava/IrisX8/patches/bt.patch
+ cd ..
+ cd net*
+ patch -p1 < ../../device/Lava/IrisX8/patches/netd2.patch
+ patch -p1 < ../../device/Lava/IrisX8/patches/netd1.patch
+ cd ..
+ cd vo*
+ patch -p1 < ../../device/Lava/IrisX8/patches/vold.patch
+ cd ../..
+ cd frame*/av
+ patch -p1 < ../../device/Lava/IrisX8/patches/av.patch
+ cd ..
+ cd base
+ patch -p1 < ../../device/Lava/IrisX8/patches/base.patch
+ cd ..
+ cd rs
+ patch -p1 < ../../device/Lava/IrisX8/patches/rs.patch
+ cd ..
+ cd native
+ patch -p1 < ../../device/Lava/IrisX8/patches/native.patch
+ cd ..
+ cd opt/te*/
+ patch -p1 < ../../../device/Lava/IrisX8/patches/telephony.patch
+ cd ../../..
+ cd exte*/sepolicy
+ patch -p1 < ../../device/Lava/IrisX8/patches/sepolicy.patch
+ cd ..
+ cd wpa*
+ patch -p1 < ../../device/Lava/IrisX8/patches/wpa.patch
+ cd ../..cd apps/Settingspatch -p1 -b < ../../../device/Lava/IrisX8/patches/dev_info.patch
+ cd ../..
+ echo Patches Applied Successfully!
