7206b2276c782ba398732b90b720dd55bfd7ba50
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-15 11:40:27 +0530

    Dissable

--- device_IrisX8.mk
+++ device_IrisX8.mk
@@ -53,6 +53,7 @@ PRODUCT_COPY_FILES += \
 # Rootdir
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
+    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6592:root/fstab.mt6592 \
     $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
     $(LOCAL_PATH)/rootdir/init.mt6592.rc:root/init.mt6592.rc \
@@ -113,7 +114,7 @@ PRODUCT_PACKAGES += \
     Snap
     
 # FM Radio
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
