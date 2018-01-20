8e49d43e8a7588a1c98b988bbb83ee2c68120ed6
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-20 19:45:34 +0530

    Update apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -2,8 +2,8 @@
 cd ../../../.. 
 cd system/core 
 patch -p1 < ../../device/Lava/IrisX8/patches/core.patch 
-cd ../.. 
-#cd packages/apps/Settings 
+cd ..
+cd packages/apps/Settings 
 $patch -p1 < ../../../device/Lava/IrisX8/patches/dev_info.patch 
 $cd ../../.. 
 cd system*/bt 
