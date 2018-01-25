2d2662dfec6d115e659f36987542820111bcff0f
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-25 14:28:47 +0530

    gui and keylayout

--- device_IrisX8.mk
+++ device_IrisX8.mk
@@ -101,6 +101,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
     $(LOCAL_PATH)/configs/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny
 
+# Keylayout overrides
+PRODUCT_COPY_FILES_OVERRIDES += \
+    system/usr/keylayout/Generic.kl
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
